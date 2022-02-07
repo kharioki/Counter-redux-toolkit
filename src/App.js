8dcc67b0ccd01eadb0e7c42279d17e5105d353dc
tony <antolisah@gmail.com>
2022-02-08 00:42:21 +0300

    redux store and createSlice actions and reducer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
+import { decrement, increment, incrementByAmount, reset } from './redux/counter';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <h1>The count is: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>
+      <button onClick={() => dispatch(increment())}>
         Increment
       </button>
-      <button onClick={() => setCount(count - 1)}>
+      <button onClick={() => dispatch(decrement())}>
         Decrement
+      </button>
+      <button onClick={() => dispatch(incrementByAmount(45))}>
+        Increment by 45
+      </button>
+      <button onClick={() => dispatch(reset())}>
+        Reset
       </button>
     </div>
   );
